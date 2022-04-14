8225c83923642f20e33ec09792c79c2a04028d42
Mucahid Yazar <mucahidyazar@gmail.com>
2022-04-14 09:49:25 +0300

    fix: hotfix for unuseless import

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import pluginRewriteAll from 'vite-plugin-rewrite-all';
 import path from 'path';
 
 // https://vitejs.dev/config/
